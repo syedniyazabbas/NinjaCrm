417b7f798a1bcb6eb5edfcc568ca3261a12bec27
Abc <Abc@DESKTOP-8M4K0N5>
2026-01-28 17:34:09 +0530

    first comment

---
+++ pushpull/src/test/java/pushpull/Firstpush.java
@@ -0,0 +1,12 @@
+package pushpull;
+
+public class Firstpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("first comment program");
+
+	}
+
+}
